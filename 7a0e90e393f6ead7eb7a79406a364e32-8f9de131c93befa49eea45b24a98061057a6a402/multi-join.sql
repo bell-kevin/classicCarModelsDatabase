924d33c426363870175e4c2854e7db9d72cbc81c
Kevin Bell <bellKevin@pm.me>
2022-12-08 15:27:05 -0700

    Add files via upload

---
+++ 7a0e90e393f6ead7eb7a79406a364e32-8f9de131c93befa49eea45b24a98061057a6a402/multi-join.sql
@@ -0,0 +1,15 @@
+
+SELECT p.productName, pl.productLine, c.customerName 
+FROM productlines AS pl INNER JOIN products AS p 
+ON pl.productLine = p.productLine 
+INNER JOIN orderdetails AS od 
+USING (productCode) 
+INNER JOIN orders AS o 
+USING (orderNumber) 
+INNER JOIN customers AS c 
+USING (customerNumber) 
+INNER JOIN employees AS e 
+ON c.salesRepEmployeeNumber = e.employeeNumber 
+INNER JOIN offices AS off 
+USING (officeCode) 
+WHERE pl.productLine = 'Planes' AND off.city = 'Sydney';
